a1f66cc56d33cfa56807de9850e8f07ad455dafa
李智慧 <lizhihui0215@gmail.com>
2017-07-03 01:31:59 +0800

    refine code

--- src/app/theme/components/baPageTop/baPageTop.component.ts
+++ src/app/theme/components/baPageTop/baPageTop.component.ts
@@ -1,30 +1,30 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
-import {GlobalState} from '../../../global.state';
+import { GlobalState } from '../../../global.state';
 
 @Component({
   selector: 'ba-page-top',
   templateUrl: './baPageTop.html',
-  styleUrls: ['./baPageTop.scss']
+  styleUrls: ['./baPageTop.scss'],
 })
 export class BaPageTop {
 
-  public isScrolled:boolean = false;
-  public isMenuCollapsed:boolean = false;
+  isScrolled: boolean = false;
+  isMenuCollapsed: boolean = false;
 
-  constructor(private _state:GlobalState) {
+  constructor(private _state: GlobalState) {
     this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
       this.isMenuCollapsed = isCollapsed;
     });
   }
 
-  public toggleMenu() {
+  toggleMenu() {
     this.isMenuCollapsed = !this.isMenuCollapsed;
     this._state.notifyDataChanged('menu.isCollapsed', this.isMenuCollapsed);
     return false;
   }
 
-  public scrolledChanged(isScrolled) {
+  scrolledChanged(isScrolled) {
     this.isScrolled = isScrolled;
   }
 }
